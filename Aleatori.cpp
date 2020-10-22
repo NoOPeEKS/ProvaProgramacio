9f106d06db088a9bab9c1a4223b9bbc49a994c54
Arnau <arnau.berenguerj@e-campus.uab.cat>
2020-10-22 19:15:36 +0200

    Primer Upload

---
+++ Aleatori.cpp
@@ -0,0 +1,4 @@
+int Aleatori(int num1){
+    int num = num1;
+    return num;
+}
---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Aleatori.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!!!" << endl;
+    cout << "El numero aleatori és: " << Aleatori(76);
+    return 0;
+}
